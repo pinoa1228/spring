6099fd7bf78a9eb5e292d0f85ddda2f0923e6ae5
stella <pinoa1228@gmail.com>
2021-06-03 10:21:31 +0900

    유저 디티오 추가

--- src/main/java/com/jts/springboot/web/dto/ConsultantDto.java
+++ src/main/java/com/jts/springboot/web/dto/ConsultantDto.java
@@ -4,6 +4,7 @@ package com.jts.springboot.web.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jts.springboot.domain.entity.ConsultantEntity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+
 public class ConsultantDto {
     private Long consultant_num;
     private String name;
--- src/main/java/com/jts/springboot/web/dto/UserDto.java
+++ src/main/java/com/jts/springboot/web/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.jts.springboot.web.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jts.springboot.domain.entity.UserEntity;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDto {
    private Long personal_num;
     private String id;
@@ -20,8 +22,7 @@ public class UserDto {
     private String name;
     private Long consultant_num;
 
-    public UserDto() {
-    }
+
     public UserDto(
                    @JsonProperty("id") String id, @JsonProperty("pw") String pw,
                    @JsonProperty("name") String name){
