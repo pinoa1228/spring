83d75249a8f17a419db97a6b464e1908954a2605
stella <pinoa1228@gmail.com>
2021-06-04 14:35:17 +0900

    예비

--- src/main/java/com/jts/springboot/web/dto/UserService.java
+++
@@ -1,9 +0,0 @@
-/*package com.jts.springboot.web.dto;
-
-import com.jts.springboot.domain.repository.UserRepository;
-
-public class UserService {
-    public static boolean checkIdDuplicate(String id){
-        return UserRepository.existsById(id);
-    }
-}*/
