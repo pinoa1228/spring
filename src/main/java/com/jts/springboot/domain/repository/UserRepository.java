a50166a1625fff1635b14e4ae8b5a63ce5e14e7f
stella <pinoa1228@gmail.com>
2021-06-06 02:10:21 +0900

    도전

--- src/main/java/com/jts/springboot/domain/repository/UserRepository.java
+++ src/main/java/com/jts/springboot/domain/repository/UserRepository.java
@@ -14,7 +14,7 @@ public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     UserEntity findById(String id);
   //  boolean existsById(String id);
-    @Query("UPDATE user p SET p.consultant_num = :consultant_num WHERE p.personal_num = :personal_num")
+    @Query(value = "UPDATE user p SET p.consultant_num = :consultant_num WHERE p.personal_num = :personal_num" ,nativeQuery = true)
     UserEntity updatenum(Long personal_num,Long consultant_num);
 
 }
