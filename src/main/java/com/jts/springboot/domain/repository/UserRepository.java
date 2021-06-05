e3cfa5a6fc6e33a4a489bd0bb9c0e368a7c967f5
stella <pinoa1228@gmail.com>
2021-06-06 02:33:47 +0900

    도전

--- src/main/java/com/jts/springboot/domain/repository/UserRepository.java
+++ src/main/java/com/jts/springboot/domain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.jts.springboot.domain.repository;
 
 import com.jts.springboot.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,6 +15,8 @@ public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     UserEntity findById(String id);
   //  boolean existsById(String id);
+
+    @Modifying(clearAutomatically = true)
     @Query(value = "UPDATE user p SET p.consultant_num = ?2 WHERE p.personal_num = ?1" ,nativeQuery = true)
     UserEntity updatenum(Long personal_num,Long consultant_num);
 
